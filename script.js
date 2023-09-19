8f2389566a9a4bc8a54378f30ec3c735bc75ba0f
pinhaosama <gabrielpinhao1@gmail.com>
2023-09-18 20:55:21 -0700

    add footer

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function menuCollapse() {
     document.getElementById("ham").checked = false;
 }
+
+function footerCollapse(element) {
+    let menu = document.getElementsByClassName('footerList');
+    let check = false;
+    if(check == false) {
+        menu.style.display = "block";
+        check = true;
+    }else{
+        menu.style.height = "none";
+        check = false;
+    }
+}
